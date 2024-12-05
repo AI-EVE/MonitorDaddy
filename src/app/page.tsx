f4b0fcbc790d48cae147a555307eb1138ecd928a
Hussam Eid <hussampc50@gmail.com>
2024-12-05 23:13:04 +0200

    first Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Page = () => {
+  return (
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper></MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  )
+}
+
+export default Page
